e207ced7b7da6c87305a177a35cd594d51920147
Paulo <paulormartins12@gmail.com>
2017-11-24 18:47:17 -0200

    Update PerceptronJava.java

--- PerceptronJava.java
+++ PerceptronJava.java
@@ -1,47 +1,95 @@
-import java.util.*;
 
-public class PerceptronJava {
+public class Perceptron {
+		
+		int[][] input_data = new int[4][3];
 
-	double[] pesos;
-	double limite;
-	
-	public void Treinamento(double [][] input_data, int[] output_data, double rate, double epoca, double limite) {
-		this.limite = limite;
-		int x = input_data[0].length;
-		int p = output_data.length;
-		/*Sorteio aleatorio de pesos*/
-		pesos = new double [x];
-		Random n = new Random();
-		
-		for(int i=0; i<x;i++) {	
-			pesos[i]=n.nextDouble();
-		}
-		for(int i=0; i<epoca;i++) {
-			int erro_total = 0;
-			for(int j=0;j<epoca;j++) {
-				int saida = Saida(input_data[j]);
-				int erro = output_data[j]-saida;
-				erro_total += erro;
-					for(int k=0;k<x;k++) {
-						double alfa = rate * input_data[j][k] * erro_total;
-						pesos[k] += alfa;
-					}
-			}
-		}
-		
+	    double[] weight = new double[3]; 
+	    double theta = 0;
+	    int max_int = 1000;
+	    int sum = 0;
+	    double learning_rate=0.5;
+	    
+	  Perceptron() {
+		/*valores inicias da matriz = dados de entrada da porta logica and*/  
+	    this.input_data[0][0] = 0; 
+	    this.input_data[0][1] = 0;
+	    this.input_data[0][2] = 0; // valor de saida esperado
+
+	    this.input_data[1][0] = 0;
+	    this.input_data[1][1] = 1;
+	    this.input_data[1][2] = 0; // valor de saida esperado
+
+	    this.input_data[2][0] = 1; 
+	    this.input_data[2][1] = 0; 
+	    this.input_data[2][2] = 0; // valor de saida esperado
+
+	   
+	    this.input_data[3][0] = 1; 
+	    this.input_data[3][1] = 1; 
+	    this.input_data[3][2] = 1;// valor de saida esperado
+
+	    weight[0] = 0;
+	    weight[1] = 0;
+	    weight[2]= 0;
+
 	}
-	
-		public int Saida(double[] input_data) {
-			double soma = 0;
-			for(int i=0; i<input_data.length;i++) {
-				soma+=pesos[i]*input_data[i];
-			}
-			if(soma>limite)
-				return 1;
-			else
-				return 0;
-		}
-			
-}
+	  //função de ativação
+	    int activation(int x1, int x2) {
 
+	       
+	       theta = (x1 * weight[0]) + (x2 * weight[1]) + (weight[2]);
 
+	        if (theta > 0) {
+	            return 1;
+	        }
+	        return 0;
+	    }
+
+	  //Treinamento do algoritmo
+	    public void trainning() {
+
+	        boolean trained= true;
+	        int saida;
+
+	        for (int i = 0; i < 4; i++) {
+	            saida = activation(input_data[i][0], input_data[i][1]);
+	            if (saida != input_data[i][2]) { 
+	                weightUpdate(i, saida);
+	                trained = false;
+	            }
+	        }
+	        this.sum++;
+
+	        if((trained == false) && (this.sum< this.max_int)) {
+	            trainning();
+	        }
+
+	    }    
+	    // Atualizacao de valores dos pesos
+	    void weightUpdate(int i, int saida) {
+
+	    	weight[0] = weight[0] + ((input_data[i][2] - saida) * input_data[i][0]*learning_rate);
+	    	weight[1] = weight[1] + ( (input_data[i][2] - saida) * input_data[i][1]*learning_rate);
+	    	weight[2] = weight[2] + ( (input_data[i][2] - saida)*learning_rate);
+
+	    }
+
+	    void print() {
+
+	    		System.out.println( "==== AND Logical Gate ===");
+	        System.out.println("0 e 0 = " + activation(0, 0));
+	        System.out.println("0 e 1 = " + activation(0, 1));
+	        System.out.println("1 e 0 = " + activation(1, 0));
+	        System.out.println("1 e 1 = " + activation(1, 1));
+	        System.out.println( "=========================");
+
+	    }
+
+	    public static void main(String[] arguments) {
+
+	        Perceptron p = new Perceptron();
+	        p.trainning();
+	        p.print();
+
+	    }
+	}
